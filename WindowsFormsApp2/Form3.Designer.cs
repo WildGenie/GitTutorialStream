257b5427ca5b486f82f2334e579eefe7927be939
Bilgehan Zeki ÖZAYTAÇ <zekiozaytac@hotmail.com.tr>
2019-08-21 21:29:29 +0300

    Form3'e Button Eklendi.

--- WindowsFormsApp2/Form3.Designer.cs
+++ WindowsFormsApp2/Form3.Designer.cs
@@ -28,12 +28,32 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.metroButton1 = new MetroFramework.Controls.MetroButton();
+      this.SuspendLayout();
+      // 
+      // metroButton1
+      // 
+      this.metroButton1.Location = new System.Drawing.Point(156, 196);
+      this.metroButton1.Name = "metroButton1";
+      this.metroButton1.Size = new System.Drawing.Size(171, 23);
+      this.metroButton1.TabIndex = 0;
+      this.metroButton1.Text = "metroButton1";
+      this.metroButton1.UseSelectable = true;
+      // 
+      // Form3
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.metroButton1);
+      this.Name = "Form3";
       this.Text = "Form3";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private MetroFramework.Controls.MetroButton metroButton1;
   }
 }
